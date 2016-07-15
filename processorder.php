c7e860a56aa957bf5ed7653481417f217db63d52
e.pol <eugene.poletski@gmail.com>
2016-07-15 14:38:50 +0300

    Add report infoto process script

--- processorder.php
+++ processorder.php
@@ -2,12 +2,24 @@
 <html>
   <head>
     <title>Bob`s Auto Parts - Order Results</title>
+	<?php
+	  // create sho variable names
+	  $tireqty  = $_POST['tireqty'];
+	  $oilqty   = $_POST['oilqty'];
+	  $sparkqty = $_POST['sparkqty'];
+	?>
   </head>
   <body>
     <h1>Bob`s Auto Parts</h1>
 	<h2>Order Results</h2>
 	<?php
-	  echo '<p>Order Processed.</p>' ;
+	  echo '<p>Order Processed at ' ;
+	  echo date('H:i, jS F Y');
+	  echo '</p>';
+	  echo '<p>Your order is as follows: </p>';
+	  echo $tireqty.' tires<br />';
+	  echo $oilqty.' bottles of oil<br />';
+	  echo $sparkqty.' spark plugs<br />';
 	?>
   </body>
 </html>
